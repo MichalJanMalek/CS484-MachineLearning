f3e43f41b4a2aac0e9c6104345709ae7dc4cbfb2
Michal Malek <malek.michal.j@gmail.com>
2022-08-29 16:22:12 -0500

    Initial commit

---
+++ Assignment_1/Assignment1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 29 15:59:16 2022
+
+@author: michalmalek
+"""
+
+import pandas as ns
+
+df = ns.read_csv('NormalSample.csv')
+
+print(df.to_string()) 
+
